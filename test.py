fe1794fc59e42b1f912c271dc84df11c7e408e72
早新 <ohiyox@gmail.com>
2023-01-26 21:03:52 +0800

    update

---
+++ test.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/1/26 20:59
+# @Author  : Chris.Wang
+# @Site    : 
+# @File    : test.py
+# @Software: PyCharm
+# @Description:
+
+import unittest
+
+
+class TestDB(unittest.TestCase):
+    def test_db(self):
+        pass
+
